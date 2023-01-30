fb83900849839d1588f56115dd2a5402f63e44a0
catarinaoliveira1200739 <1200739@isep.ipp.pt>
2023-01-30 15:19:09 +0000

    Base do sensor a funcionar

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -6,28 +6,10 @@
 
 #define CFG_DEBUG                         true
 #define CFG_DEBUG_BAUDRATE                115200
+#define CFG_BUFFER_SIZE                   1024
 
-#define CFG_NUM_SENSORES                  5
-
-#define CFG_SENSOR1_INDEX                 0
-#define CFG_SENSOR1_NOME                  "Frequencia Cardiaca"
-#define CFG_SENSOR1_UNIDADE               "bps"
-
-#define CFG_SENSOR2_INDEX                 1
-#define CFG_SENSOR2_NOME                  "Oxigenio no Sangue"
-#define CFG_SENSOR2_UNIDADE               "%"
-
-#define CFG_SENSOR3_INDEX                 2
-#define CFG_SENSOR3_NOME                  "Pressao arterial min"
-#define CFG_SENSOR3_UNIDADE               "bps"
-
-#define CFG_SENSOR3_INDEX                 3
-#define CFG_SENSOR3_NOME                  "Pressao arterial max"
-#define CFG_SENSOR3_UNIDADE               "bps"
-
-#define CFG_SENSOR3_INDEX                 4
-#define CFG_SENSOR3_NOME                  "Detetor Quedas"
-#define CFG_SENSOR3_UNIDADE               ""
+#define CFG_SERVICE_ADDRESS               "http://localhost:8000/amostras/"
+#define CFG_DATABASE_ID_RELOGIO           1
 
 #define CFG_TAXA_AMOSTRAGEM_SENSORES_MS   1000
 
@@ -86,21 +68,119 @@ void wifi_main(){
   Estruturas de dados dos sensores
 */
 typedef struct amostra_t{
-  char nomeSensor[100];
+  char* nomeSensor;
   float valor;
-  char unidade[100];
+  char* unidade;
   bool lido;
 }amostra;
-amostra sensores[CFG_NUM_SENSORES];
+amostra sensores[] = {
+  {
+    "Frequencia Cardiaca",
+    0.0,
+    "bps"
+    false
+  },
+  {
+    "Oxigenio no Sangue",
+    0.0,
+    "%"
+    false
+  },
+  {
+    "Pressao arterial min",
+    0.0,
+    "bps",
+    false
+  },
+  {
+    "Pressao arterial max",
+    0.0,
+    "bps",
+    false
+  },
+  {
+    "Detetor Quedas",
+    0.0,
+    "N",
+    false
+  }
+}
+
+char* amostra_to_json(amostra* m,char* buff){
+  sprintf(buff,"
+  {
+    \"sensor\": %d,
+    \"unidade\": \"%s\",
+    \"valor\": %f
+  }
+  ", CFG_DATABASE_ID_RELOGIO, m->unidade, m->valor);
+  return buff;
+}
+
+void amostra_post(char* httpRequestData){
+  HTTPClient http;
+  char* serverPath = CFG_SERVICE_ADDRESS;
+  http.begin(serverPath);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(httpRequestData);
+  if(httpResponseCode != 200){
+    debug_print("Erro de ligacao");
+  }
+  http.end();
+}
+
+void amostra_main(){
+  int i;
+  char buffer[CFG_BUFFER_SIZE];
+
+  if(wifi_ligado == false)
+    return;
+
+  for(i=0;i< sizeof(sensores) / sizeof(sensores[0]); i++){
+      if(sensores[i].lido == true){
+        amostra_post(amostra_to_json(sensores[i],buffer));
+        sensores[i].lido = false;
+      }
+  }
+
+}
+
+typedef struct tarefa_t{
+  long int t_inicio_ms;
+  long int t_perido_ms
+  void(*f)(void) t;
+}tarefa;
+
+void sensor_freq_cardiaca_main(){
+  //leu-se 50
+  sensores[0].valor = sensores[0].valor + 1;
+  sensores[0].lido = true;
+
+}
+
+
+tarefa tarefas[] = {
+// t        periodo Funcao
+  { 0,      500,    wifi_main                   },
+  { 0,      500,    amostra_main                },
+  { 0,      1000,   sensor_freq_cardiaca_main   }
+}
 
 
 
-
+void tarefas_main(){
+  int i;
+  for(i=0;i<sizeof(tarefas) / sizeof(tarefas[0]), i++){
+      if(millis() - tarefas[i].t_inicio_ms >= tarefas[i].t_periodo_ms){
+        tarefas[i].t();
+        tarefas[i].t_inicio_ms = millis();
+      }
+  }
+}
 void setup() {
   wifi_init();
 }
 
 void loop() {
-  wifi_main();
-
+  tarefas_main();
 }
